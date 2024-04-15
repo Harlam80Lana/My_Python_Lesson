a2bd83c3ca2122181619decb4086184dd66d57e2
Svetlana <153324028+Harlam80Lana@users.noreply.github.com>
2024-04-15 18:15:17 +0300

    lesson1

---
+++ lesson1/lesson1_task4.py
@@ -0,0 +1,12 @@
+# вариант 1
+
+first_name = "Svetlana"
+last_name = "Alekseevna"
+print("My name is: " + first_name, last_name)
+
+
+# вариант 2
+
+first_name = input("Your name?")                                       #Ответ: Svetlana
+last_name = input("Your patronymic?")                              #Ответ: Alekseevna
+print(f"My name is: {first_name} {last_name}")
---
+++ lesson1/lesson1_task6.py
@@ -0,0 +1,42 @@
+def print_num1():
+    print(1, end = "")
+
+def print_num2():
+    print(2, end = "")
+
+def print_num3():
+    print(3, end = "")
+
+def print_num4():
+    print(4, end = "")
+
+def print_num5():
+    print(5, end = "")
+
+def print_num_5():
+    print(5, end = "-")
+
+def print_num6():
+    print(6, end = "")
+
+def print_num7():
+    print(7, end = "")
+
+def print_num8():
+    print(8, end = "")
+
+def print_num_8():
+    print(8, end = "-")
+
+def print_num9():
+    print(9, end = "")
+
+def print_num0():
+    print(0, end = "")
+
+def print_num_0():
+    print(0, end = "-")
+
+
+print_num_8(), print_num8(), print_num0(), print_num_0(), print_num5(), print_num5(),
+print_num_5(), print_num3(), print_num_5(), print_num3(), print_num5() 
